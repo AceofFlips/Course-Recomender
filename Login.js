7479aacee109505d34d3b3785d81e919cc2ed636
owilling <owilling@ftstudent.org>
2026-04-24 08:10:01 -0400

    Verified Credentials in login page
    **Not very secure just a basic text file

--- Login.js
+++ Login.js
@@ -1,6 +1,36 @@
+//Do something after login is verified and add session/local storage login token
+
 var sumbitForm = document.getElementById("login-form");
 sumbitForm.addEventListener("submit", function(event) {
     event.preventDefault();
     var input = $("#login-form").serializeArray();
-    console.log(input);
+    var canLogin = verifyLogin(input[0].value, input[1].value);
+    if (canLogin) {
+        console.log("Valid Login Credentials");
+    } else {
+        console.log("Invalid Login Credentials")
+    }
 })
+
+async function verifyLogin(username, password) {
+    try {
+        //get user credentials file
+        const response = await fetch('users.txt');
+        const text = await response.text();
+        //trim of edges
+        const lines = text.split(/\r?\n/);
+        //iterate through lines
+        for (let line of lines) {
+            //split into username and password
+            const [storedUser, storedPass] = line.split(':');
+            //check to see if crednetials match
+            if (storedUser === username && storedPass === password) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    } catch (err) {
+        console.error("Error reading file:");
+    }
+}
