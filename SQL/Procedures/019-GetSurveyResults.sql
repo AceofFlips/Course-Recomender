329f9d8a93d21e2af6214de3a3e472451405362a
Ace of Flips <174635729+AceofFlips@users.noreply.github.com>
2026-05-08 08:50:17 -0400

    Finished the Survey Query
    +Finished the backend query for the survey

--- SQL/Procedures/019-GetSurveyResults.sql
+++ SQL/Procedures/019-GetSurveyResults.sql
@@ -1,7 +1,34 @@
-CREATE PROCEDURE IF NOT EXISTS(
-
+CREATE PROCEDURE IF NOT EXISTS GetSurveyResults(
+    Path NVARCHAR(12),
+    Credit NVARCHAR(12),
+    IsCC TINYINT(1)
 )
 BEGIN
-    SELECT Display.class_name FROM Courses AS Display
-    ORDER BY ;
+    SELECT  Paths.id, Paths.course_name, Paths.description
+    FROM    (SELECT Display.id, Display.course_name, Display.description,
+                    ROW_NUMBER() OVER (PARTITION BY Display.course_name ORDER BY Display.id) AS RowNumber
+             FROM Courses AS Display JOIN CoursesPathways
+             WHERE Display.id = CoursesPathways.c_id
+               AND Display.public = 1
+               AND FIND_IN_SET(CoursesPathways.path_id,Path)) AS Paths
+    WHERE   Paths.RowNumber = 1
+    UNION
+    SELECT Cred.id, Cred.course_name, Cred.description
+    FROM (SELECT Display.id, Display.course_name, Display.description,
+                 ROW_NUMBER() OVER (PARTITION BY Display.course_name ORDER BY Display.id) AS RowNumber
+          FROM Courses AS Display JOIN CoursesCredits
+          WHERE Display.id = CoursesCredits.c_id
+            AND Display.public = 1
+            AND FIND_IN_SET(CoursesCredits.cred_id,Credit)) AS Cred
+    WHERE Cred.RowNumber = 1
+    UNION
+    SELECT Difficulty.id, Difficulty.course_name, Difficulty.description
+    FROM Courses AS Difficulty
+    WHERE Difficulty.CC = IsCC
+        AND Difficulty.public = 1
+    UNION
+    SELECT Display.id, Display.course_name, Display.description
+    FROM Courses AS Display
+    WHERE Display.public = 1;
+
 end;
--- SQL/test.sql
+++ SQL/test.sql
@@ -1,8 +1,28 @@
 /* used to test queries; do not run unless testing */
-
-SELECT DISTINCT Display.course_name, CoursesPathways.path_id FROM Courses AS Display JOIN CoursesPathways
-         WHERE Display.id = CoursesPathways.c_id
-         AND CoursesPathways.path_id IN (1,3,5,7,9,11,13,15,17,19,2,4,6,8,10,12,14,16)
-        ORDER BY FIND_IN_SET(path_id,'1,3,5,7,9,11,13,15,17,19,2,4,6,8,10,12,14,16');
-
 /* Research DISTINCT and UNION */
+
+-- Source - https://stackoverflow.com/a/966215
+-- Posted by Aaron Alton
+-- Retrieved 2026-05-08, License - CC BY-SA 2.5
+
+SELECT  Paths.id, Paths.course_name, Paths.description
+FROM    (SELECT Display.id, Display.course_name, Display.description,
+                ROW_NUMBER() OVER (PARTITION BY Display.course_name ORDER BY Display.id) AS RowNumber
+         FROM Courses AS Display JOIN CoursesPathways
+         WHERE Display.id = CoursesPathways.c_id
+           AND FIND_IN_SET(CoursesPathways.path_id,'4')
+           AND Display.public = 1) AS Paths
+WHERE   Paths.RowNumber = 1
+UNION
+SELECT Cred.id, Cred.course_name, Cred.description
+FROM (SELECT Display.id, Display.course_name, Display.description,
+             ROW_NUMBER() OVER (PARTITION BY Display.course_name ORDER BY Display.id) AS RowNumber
+      FROM Courses AS Display JOIN CoursesCredits
+      WHERE Display.id = CoursesCredits.c_id
+        AND FIND_IN_SET(CoursesCredits.cred_id,'2,3')
+        AND Display.public = 1) AS Cred
+WHERE Cred.RowNumber = 1
+UNION
+SELECT Display.id, Display.course_name, Display.description
+FROM Courses AS Display
+WHERE Display.public = 1;
